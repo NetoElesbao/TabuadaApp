d8e597d1317fb3971df2747882549eab637f16ab
José Waldir de Assis Elesbão Neto <est_jelesbao@banparanet.com.br>
2024-02-26 16:58:16 -0300

    V2.0

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,24 @@
-console.log("Olá");
+// Seleção de elementos
+const multiplicationform = document.querySelector("#multiplication-form");
+const numberInput = document.querySelector("#number");
+const multiplicationInput = document.querySelector("#multiplicator");
+
+console.log(multiplicationform);
+console.log(numberInput);
+console.log(multiplicationInput);
+
+// Funções
+multiplicationform.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const multiplicatonNumber = +numberInput.value;
+  const multiplicatorNumber = +multiplicationInput.value;
+
+  if (!multiplicatonNumber || !multiplicatorNumber) {
+    return;
+  }
+
+  console.log(multiplicatonNumber, multiplicatorNumber);
+});
+
+// Eventos
